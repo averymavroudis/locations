4667b3eb1e578ed4260d07b3b99fee76571fce1f
Avery <avery.mavroudis@epifany.com>
2018-04-05 17:13:21 -0400

    Fixing Formatting

--- clustering.R
+++ clustering.R
@@ -3,18 +3,18 @@ library(utils)
 
 clustering <- function(locations) {
   locations <- na.omit(locations)
-  x <- as.matrix(locations[,2:3])
+  x <- as.matrix(locations[, 2:3])
   db <- dbscan(x, eps = .1)
   
   locations$cluster <- db$cluster
   clusterLatitudes <- with(locations, tapply(Latitude, cluster, mean))
   clusterLongitudes <-with(locations, tapply(Longitude, cluster, mean))
-  clusterCoordinate <- as.table(cbind(clusterLatitudes,clusterLongitudes))
+  clusterCoordinate <- as.table(cbind(clusterLatitudes, clusterLongitudes))
   
-  clustcenters <- as.data.frame(table(db$cluster))
-  ClusterNumber <- clustcenters$Var1
-  ClusterDensity <- clustcenters$Freq
+  clustCenters <- as.data.frame(table(db$cluster))
+  clusterNumber <- clustCenters$Var1
+  clusterDensity <- clustCenters$Freq
   
-  output <- cbind(ClusterDensity,clusterLatitudes,clusterLongitudes)
+  output <- cbind(clusterDensity, clusterLatitudes, clusterLongitudes)
   as.data.frame(output)
 }
