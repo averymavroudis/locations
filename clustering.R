f8c59f2363dd019ce071d17451844e2f68a1ce12
Avery <avery.mavroudis@epifany.com>
2018-04-10 13:26:18 -0400

    Added lintr to fix style issues

--- clustering.R
+++ clustering.R
@@ -4,16 +4,16 @@ clustering <- function(locations) {
   locations <- na.omit(locations)
   x <- as.matrix(locations[, 2:3])
   db <- dbscan(x, eps = .1)
-  
+
   locations$cluster <- db$cluster
   clusterLatitudes <- with(locations, tapply(Latitude, cluster, mean))
-  clusterLongitudes <-with(locations, tapply(Longitude, cluster, mean))
+  clusterLongitudes <- with(locations, tapply(Longitude, cluster, mean))
   clusterCoordinate <- as.table(cbind(clusterLatitudes, clusterLongitudes))
-  
+
   clustCenters <- as.data.frame(table(db$cluster))
   clusterNumber <- clustCenters$Var1
   clusterDensity <- clustCenters$Freq
-  
+
   output <- cbind(clusterDensity, clusterLatitudes, clusterLongitudes)
   as.data.frame(output)
 }
