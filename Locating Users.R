16c259138170ac36ebcf8ea268f658384883e120
Avery <avery.mavroudis@epifany.com>
2018-04-10 16:15:52 -0400

    Removed initializing packages to other source and style formatting

--- Locating Users.R
+++ Locating Users.R
@@ -1,27 +1,18 @@
-list.of.packages <- c("geosphere","dbscan","data.table","utils")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-library("data.table")
-filename <- "~/Documents/Stealz/Determining User's Central Location/Users's Locations - Locations.csv"
-OutputFileName <- "~/Documents/Stealz/Determining User's Central Location/thisisatest.csv"
 source("./Clustering.R")
+source("./init.R")
 
-ClusterMax <- function(userlocations){
+maxCluster <- function(userlocations){
   v <- clustering(userlocations)
-  as.data.frame(v[which.max(v$ClusterDensity),])
-  
+  as.data.frame(v[which.max(v$clusterDensity), ])
 }
 
-userlocations <- read.csv(filename, header = TRUE)
-
-
 locationsCSV <- function(data, fileName = ""){
-  data <- sapply(split(data, data$Id),ClusterMax)
+  data <- sapply(split(data, data$id), maxCluster)
   data <- t(data)
-  lat <- unlist(data[,2])
-  lon <- unlist(data[,3])
-  data <- as.data.frame(cbind(lat,lon))
-  setDT(data,keep.rownames = TRUE)
-  colnames(data) <- c("UserID","Latitude","Longitude")
-  write.csv(data,file = fileName, row.names = FALSE)
+  lat <- unlist(data[, 2])
+  lon <- unlist(data[, 3])
+  data <- as.data.frame(cbind(lat, lon))
+  setDT(data, keep.rownames = TRUE)
+  colnames(data) <- c("UserID", "Latitude", "Longitude")
+  write.csv(data, file = fileName, row.names = FALSE)
 }
